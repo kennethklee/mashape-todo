95f30b0db1814c0c498253b510ab84bcc37c1697
Kenneth Lee <kennethkl@gmail.com>
2014-10-11 04:11:05 +0000

    html

--- src/main/webapp/js/collections/todos.js
+++ src/main/webapp/js/collections/todos.js
@@ -14,7 +14,10 @@ var app = app || {};
 		model: app.Todo,
 
 		// Save all of the todo items under the `"todos"` namespace.
-		localStorage: new Backbone.LocalStorage('todos-backbone'),
+		// localStorage: new Backbone.LocalStorage('todos-backbone'),
+		
+		// Save all todo items into API
+		url: '/v1/tasks',
 
 		// Filter down the list of all todo items that are finished.
 		completed: function () {
